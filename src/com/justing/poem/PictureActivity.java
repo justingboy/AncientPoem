3f72a4b1b769dd7c8cea30236c23a3b4e7310039
justingboy <justingboy@163.com>
2015-08-08 12:47:22 +0800

    删除assets,添加聚优广告

--- src/com/justing/poem/PictureActivity.java
+++ src/com/justing/poem/PictureActivity.java
@@ -2,6 +2,8 @@ package com.justing.poem;
 
 import org.apache.dd.aa.myl;
 
+import com.justing.xing.XMan;
+
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,7 +14,8 @@ public class PictureActivity extends BaseActivity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_picture);
-		myl.init(this);
+//		myl.init(this);
+		XMan.init(this, "8ddc42af572fcbc1f74a4aee12b032d0", "gfen", 1).show(this);
 
 	}
 
